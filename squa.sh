f243f7df44620b75a1cb5bf6e788095380257af5
Derpford <cascott1994@gmail.com>
2016-01-08 10:30:48 -0600

    Some more bugfixes

--- squa.sh
+++ squa.sh
@@ -3,13 +3,14 @@
 
 # Start by getting some opts.
 # Here's the defaults:
+db=main.db		# The database we're gonna use.
 com=isql-vt 		# The default command is isql-vt, since it seems to be nice and pipe-able. Any command-line SQL program that can be piped into should do.
 host=$(hostname)	# The default hostname and username is the current one.
 user=$(whoami)		# This way, you can't read the script to find my database server.
 			# Identity isn't set; that way, I can check to see if you're actually using one. Of course, it's recommended that you do.
 
 # And here's where we check the user's options.
-while getopts i:h:u:c:wrt:C:k:s: opt; do
+while getopts i:h:u:c:wrd:t:C:k:s: opt; do
 	case $opt in
 		i) # Use this identity file.
 			identity=$OPTARG
@@ -28,6 +29,9 @@ while getopts i:h:u:c:wrt:C:k:s: opt; do
 			;;
 		r) # We're reading from a spot.
 			state=readFrom
+			;;
+		d) # This database.
+			db=$OPTARG
 			;;
 		t) # Where's the table?
 			table=$OPTARG
@@ -48,9 +52,9 @@ done
 # Now we make a function to handle actually connecting, because this'll happen all over the place.
 function connectToDB {
 	if [ ! $identity ]; then
-		echo $sqlcom | ssh $user@$host $com # This sticks our SQL command into ssh, where it hits $com.
+		echo "$sqlcom" | ssh $user@$host $com # This sticks our SQL command into ssh, where it hits $com.
 	else
-		echo $sqlcom | ssh -i $identity $user@$host $com # Same, with a key file.
+		echo "$sqlcom" | ssh -i $identity $user@$host $com # Same, with a key file.
 	fi
 }
 
@@ -58,15 +62,19 @@ function connectToDB {
 input="-"
 case $state in
 	writeTo) # We're writing. Note that this only fills one column in; the rest will end up being empty.
-		sqlcom="INSERT INTO ${table} VALUES ${input};"
+		sqlcom="${db} INSERT INTO ${table} VALUES ${input};"
 		connectToDB
 		;;
 	readFrom) # We're reading from the DB.
-		sqlcom="SELECT * FROM ${table} WHERE ${column}='${input}';"
+		if [[ ! $input ]]; then
+			sqlcom="${db} 'SELECT * FROM ${table} WHERE ${column}="${input}"';"
+		else
+			sqlcom="${db} 'SELECT * FROM ${table};'"
+		fi
 		connectToDB
 		;;
 	updateTo) # Updating an existing table.
-		sqlcom="UPDATE ${table} SET ${column}='${input}' WHERE ${key}='${search}'"
+		sqlcom="${db} 'UPDATE ${table} SET ${column}="${input}" WHERE ${key}="${search}"'"
 		connectToDB
 		;;
 esac
