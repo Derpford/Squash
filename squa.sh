70143a9d37d02e9fb423873674b396f4b11085c4
Derpford <cascott1994@gmail.com>
2016-01-11 17:08:20 -0600

    Cleaned up $sqlcom, added default table

--- squa.sh
+++ squa.sh
@@ -7,6 +7,7 @@ db=main.db		# The default database.
 com="sqlite3 -echo" 		# The default program to run remotely. Any pipeable database command will do.
 host=$(hostname)	# The default hostname and username is the current one.
 user=$(whoami)		# This way, you can't read the script to find my database server.
+table="blog"		# The default table.
 			# Identity isn't set; that way, I can check to see if you're actually using one. Of course, it's recommended that you do.
 
 # And here's where we check the user's options.
@@ -34,21 +35,12 @@ done
 function connectToDB {
 	if [ ! $identity ]; then
 		ssh $user@$host "$com $db '$sqlcom'" # This sticks our SQL command into ssh, where it hits $com.
-		echo ssh $user@$host "$com $db '$sqlcom'"
 	else
 		ssh -i $identity $user@$host "$com $db '$sqlcom'" # Same, with a key file.
-		echo ssh -i $identity $user@$host "$com $db '$sqlcom'"
-
 	fi
 }
 
-# Here's where we're gonna make the function for assembling all the bits of data we're sticking into the DB.
 # The end result should be something along the lines of "insert into $table(title, text) values($filename, $(cat $filename)"
-function createCommand {
-	inputfile=$(cat)
-	echo "insert into $table (title, text) values ($inputfile, $(cat $inputfile));"
-}
-
+sqlcom="insert into $table (title, text) values (\"$1\", \"$(cat $1)\");"
 # Next up, the important bits.
-sqlcom=createCommand
 connectToDB
