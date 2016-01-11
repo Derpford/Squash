ece75d7ea82917acd92e4d7ae2f1db4f6e69b02c
Derpford <cascott1994@gmail.com>
2016-01-11 17:34:20 -0600

    Silly var mistake.

--- postgre/squa.sh
+++ postgre/squa.sh
@@ -3,11 +3,11 @@
 
 # Start by getting some opts.
 # Here's the defaults:
-db=main.db		# The default database.
+db=$(whoami)		# The default database.
 com="psql" 		# The default program to run remotely. Any pipeable database command will do.
 host=$(hostname)	# The default hostname and username is the current one.
 user=$(whoami)		# This way, you can't read the script to find my database server.
-table=$(whoami)		# The default table.
+table=blog		# The default table.
 			# Identity isn't set; that way, I can check to see if you're actually using one. Of course, it's recommended that you do.
 
 # And here's where we check the user's options.
